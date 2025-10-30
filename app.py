7949a9d5039a0880776f47559d2d5a0af18d1048
Fais <fais@pc161.wireless55.tku.edu.tw>
2025-10-30 17:41:35 +0800

    Resize large images to prevent memory issues

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/healthz')
+def healthz():
+    return 'OK', 200
+
 @app.route('/', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
@@ -16,6 +20,12 @@ def upload_file():
             return 'No file selected', 400
         if file:
             input_image = Image.open(file.stream)
+            
+            # Resize large images to save memory
+            max_size = 1024
+            if input_image.width > max_size or input_image.height > max_size:
+                input_image.thumbnail((max_size, max_size), Image.Resampling.LANCZOS)
+            
             output_imge = remove(input_image, post_process_mask=True)
             img_io = BytesIO()
             output_imge.save(img_io,'PNG')
@@ -27,4 +37,4 @@ def upload_file():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', debug=False, port=port)
+    app.run(host='0.0.0.0', port=port)
